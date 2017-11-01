0d727bcec78fb6c8d69b62b412a262cf33638fcf
Thomas <thomas.hiron@gmail.com>
2017-11-01 12:19:59 +0100

    Ajout de journeyapps et scan du qr code

--- app/src/main/java/com/thomas/foodleftovers/MainActivity.java
+++ app/src/main/java/com/thomas/foodleftovers/MainActivity.java
@@ -2,19 +2,42 @@ package com.thomas.foodleftovers;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.Toast;
 
-import com.thomas.foodleftovers.camera.CameraTextureView;
+import com.google.zxing.ResultPoint;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
 {
-    private static final int CAMERA = 1;
     public static final String APP_TAG = "FOOD";
+    private static final int CAMERA = 1;
+
+    private DecoratedBarcodeView barcodeView;
+
+    private BarcodeCallback callback = new BarcodeCallback()
+    {
+        @Override
+        public void barcodeResult(BarcodeResult result)
+        {
+            Log.i(APP_TAG, "Code scanné :" + result.getText());
+        }
+
+        @Override
+        public void possibleResultPoints(List<ResultPoint> resultPoints)
+        {
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -22,10 +45,17 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /* Démarrage du scan */
+        barcodeView = findViewById(R.id.barcode);
+        barcodeView.decodeContinuous(callback);
+
+        /* Suppression du texte en dessous */
+        barcodeView.getStatusView().setVisibility(View.GONE);
+
+        /* Demande de permission */
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
         {
             Log.i("FOOD", "pas de permission");
-
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA);
         }
     }
@@ -37,12 +67,14 @@ public class MainActivity extends AppCompatActivity
         {
             case CAMERA:
                 if (grantResults[0] != PackageManager.PERMISSION_GRANTED)
+                {
                     Toast.makeText(this, "Cannot run application because camera service permission have not been granted", Toast.LENGTH_SHORT).show();
+                    barcodeView.pause();
+                }
                 else
                 {
                     Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
-                    CameraTextureView tv = (CameraTextureView) findViewById(R.id.camera_texture_view);
-                    tv.openCamera();
+                    barcodeView.resume();
                 }
 
                 break;
@@ -51,5 +83,25 @@ public class MainActivity extends AppCompatActivity
                 super.onRequestPermissionsResult(requestCode, permissions, grantResults);
                 break;
         }
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        barcodeView.resume();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        barcodeView.pause();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        return barcodeView.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event);
     }
 }
