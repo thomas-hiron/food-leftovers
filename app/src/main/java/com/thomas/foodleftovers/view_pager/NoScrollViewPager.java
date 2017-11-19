a4619b64333be767d4c3ff9bc6a6662b1d76c2c8
Thomas <thomas.hiron@gmail.com>
2017-11-19 13:47:52 +0100

    Suppression du scroll du view pager

---
+++ app/src/main/java/com/thomas/foodleftovers/view_pager/NoScrollViewPager.java
@@ -0,0 +1,31 @@
+package com.thomas.foodleftovers.view_pager;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class NoScrollViewPager extends ViewPager
+{
+    public NoScrollViewPager(Context context)
+    {
+        super(context);
+    }
+
+    public NoScrollViewPager(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        return false;
+    }
+}
